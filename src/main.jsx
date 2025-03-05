2daa6247981b7f590eadc92f95e1afef38e2efae
Abdul <55804241+aqkhan070@users.noreply.github.com>
2025-03-05 19:54:10 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './MuiTheme.jsx';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
   <ThemeProvider theme={theme}>
