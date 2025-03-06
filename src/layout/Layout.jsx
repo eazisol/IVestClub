5378688ac8f10cbfdbf365305d9d6b977e6cdd18
Abdul <55804241+aqkhan070@users.noreply.github.com>
2025-03-06 14:55:20 +0500

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -14,7 +14,6 @@ const Layout = () => {
     ? JSON.parse(localStorage.getItem("userData"))
     : {access_token : ""};
 
-  
   useEffect(() => {
     setShowLandingSaction(true);
     setUserData(localuserdata);
